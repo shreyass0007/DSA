5f2c5d78c41c6bf61e1d3ddafae22d8b507ae825
shreyass0007 <shreshshende.777@gmail.com>
2025-04-18 17:59:56 +0530

    i

--- Strings/stoi_stoii.cpp
+++ Strings/stoi_stoii.cpp
@@ -4,5 +4,10 @@ using namespace std;
 int main(){
     string str="123456";
     int x=stoi(str);
-    cout<<x+1;
+    cout<<x+1<<endl;
+
+    //stoll()
+
+    long long y=stoll(str);
+    cout<<y;
 }
